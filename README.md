5c7bc1b564bf8e7236c795acacc6cb325aa458ff
evan.lightcap <evan.lightcap@cloudkitchens.com>
2023-08-18 00:24:23 -0400

    some vars

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ The main motivation for this was to add a method to get cover art for songs that
     - APIKEY - lastfm api key (can be obtained [here](https://www.last.fm/login?next=%2Fapi%2Faccount%2Fcreate%3F_pjax%3D%2523content))
     - TEMPCOVER - location where cover art is stored & read (can be anywhere that can be read, i use /tmp/cover.png)
     - MUSICDIR - root directory of music stuff (for example /home/elightcap/Music)
+    - PLACEHOLDER - path to any picture. this is used if the metadata doesnt have a picture, and it cannot be found on LastFM.
 4. Install requirements `pip install -r requirements.txt`
 5. I have this launch with i3, so make the file executable `chmod +x mpd-notify.py`
 6. I only know my i3 stuff, so i just have `exec --no-startup-id /path/to/script/mpd-notify.py`
--- mpd-notify.py
+++ mpd-notify.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import requests 
+import requests
 import shutil
 import urllib.parse
 
@@ -15,6 +15,7 @@ PORT = getenv("PORT")
 APIKEY = getenv("APIKEY")
 TEMPCOVER = getenv("TEMPCOVER")
 MUSICDIR = getenv("MUSICDIR")
+PLACEHOLDER = getenv("PLACEHOLDER")
 
 client = MPDClient()
 client.timeout = 10
@@ -32,8 +33,8 @@ def get_current_song():
     client.disconnect()
 
 def get_album_art(title,artist,album,file):
-    ffm = system("ffmpeg -i '{dir}/{filename}' {coverfile} -y &> /dev/null".format(dir=MUSICDIR,filename=file,coverfile="/tmp.cover.png"))
-    if ffm == 256: 
+    ffm = system("ffmpeg -i '{dir}/{filename}' {coverfile} -y &> /dev/null".format(dir=MUSICDIR,filename=file,coverfile=TEMPCOVER))
+    if ffm == 256:
         print("not found")
         url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={apikey}&artist={artist}&album={album}&format=json".format(apikey=APIKEY,artist=(urllib.parse.quote(artist)), album=(urllib.parse.quote(album)))
         album_info = requests.get(url)
@@ -42,18 +43,18 @@ def get_album_art(title,artist,album,file):
         get_image = requests.get(art_url)
         image_data = get_image.content
         if get_image.status_code == 200:
-            f = open("/tmp/cover.png", "wb")
+            f = open(TEMPCOVER, "wb")
             f.write(image_data)
             f.close()
         else:
-            shutil.copyfile('/home/evan/Pictures/music.png','/tmp/cover.png')
+            shutil.copyfile(PLACEHOLDER,TEMPCOVER)
 
     song_note = "{song} - {artist}".format(song=title,artist=artist)
     print(song_note)
     send_notification(song_note)
 
 def send_notification(song_note):
-    system("dunstify -a Music --replace=27072 -t 2000 -i {albumcover} '{songinfo}'".format(albumcover="/tmp/cover.png", songinfo=song_note))
+    system("dunstify -a Music --replace=27072 -t 2000 -i {albumcover} '{songinfo}'".format(albumcover=TEMPCOVER, songinfo=song_note))
 
 
 while True:
